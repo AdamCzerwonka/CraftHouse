928151bac4d2a0ecfc17d90cc72e7c334509f315
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-01-19 18:52:08 +0100

    refactor: user registration model for better readability

--- CraftHouse.Web/Pages/Registration.cshtml.cs
+++ CraftHouse.Web/Pages/Registration.cshtml.cs
@@ -15,31 +15,7 @@ public class Registration : PageModel
     }
 
     [BindProperty]
-    public string FirstName { get; set; } = null!;
-
-    [BindProperty]
-    public string LastName { get; set; } = null!;
-
-    [BindProperty]
-    public string TelephoneNumber { get; set; } = null!;
-
-    [BindProperty]
-    public string City { get; set; } = null!;
-
-    [BindProperty]
-    public string PostalCode { get; set; } = null!;
-
-    [BindProperty]
-    public string AddressLine { get; set; } = null!;
-
-    [BindProperty]
-    public string Mail { get; set; } = null!;
-
-    [BindProperty]
-    public string Password { get; set; } = null!;
-
-    [BindProperty]
-    public string ConfirmPassword { get; set; } = null!;
+    public UserRegisterModel UserRegister { get; set; } = null!;
 
     public List<string>? ValidationErrors { get; set; }
 
@@ -51,22 +27,40 @@ public class Registration : PageModel
     {
         var user = new User()
         {
-            FirstName = FirstName,
-            LastName = LastName,
-            TelephoneNumber = TelephoneNumber,
-            City = City,
-            PostalCode = PostalCode,
-            AddressLine = AddressLine,
-            Email = Mail
+            FirstName = UserRegister.FirstName,
+            LastName = UserRegister.LastName,
+            TelephoneNumber = UserRegister.TelephoneNumber,
+            City = UserRegister.City,
+            PostalCode = UserRegister.PostalCode,
+            AddressLine = UserRegister.AddressLine,
+            Email = UserRegister.Mail
         };
 
-       var result =  await _authService.RegisterUser(user, Password);
-       if (!result.Succeeded)
-       {
-           ValidationErrors = result.Errors;
-           return Page();
-       }
+        if (UserRegister.Password != UserRegister.ConfirmPassword)
+        {
+            ValidationErrors = new List<string> { "Passwords do not match" };
+        }
+
+        var result = await _authService.RegisterUser(user, UserRegister.Password);
+        if (!result.Succeeded)
+        {
+            ValidationErrors = result.Errors;
+            return Page();
+        }
 
         return RedirectToPage("index");
     }
+}
+
+public class UserRegisterModel
+{
+    public string FirstName { get; init; } = null!;
+    public string LastName { get; init; } = null!;
+    public string TelephoneNumber { get; init; } = null!;
+    public string City { get; init; } = null!;
+    public string PostalCode { get; init; } = null!;
+    public string AddressLine { get; init; } = null!;
+    public string Mail { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public string ConfirmPassword { get; init; } = null!;
 }
