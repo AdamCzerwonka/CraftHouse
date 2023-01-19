f7e36f298d448a6561bdc2f3e4fb03ccad165eb4
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-01-19 18:27:55 +0100

    Add AuthResult class as a universal return for auth service

--- CraftHouse.Web/Services/IAuthService.cs
+++ CraftHouse.Web/Services/IAuthService.cs
@@ -4,7 +4,7 @@ namespace CraftHouse.Web.Services;
 
 public interface IAuthService
 {
-    Task RegisterUser(User user, string password);
+    Task<AuthResult> RegisterUser(User user, string password);
     bool VerifyUserPassword(User user, string password);
     bool Login(User user, string password);
     bool IsLoggedIn();
