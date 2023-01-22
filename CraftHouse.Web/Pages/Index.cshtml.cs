a80a0a4ae3d422933437ba4a4a30057a091288e7
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-01-22 01:07:35 +0100

    feat: display products on main page

--- CraftHouse.Web/Pages/Index.cshtml.cs
+++ CraftHouse.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using CraftHouse.Web.Entities;
+﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.Entities;
 using CraftHouse.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,17 +11,21 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IAuthService _authService;
+    private readonly AppDbContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger, IAuthService authService)
+    public IndexModel(ILogger<IndexModel> logger, IAuthService authService, AppDbContext context)
     {
         _logger = logger;
         _authService = authService;
+        _context = context;
     }
 
     public User? LoggedInUser { get; set; }
+    public List<Product> Products { get; set; } = null!;
 
     public void OnGet()
     {
         LoggedInUser = _authService.GetLoggedInUser();
+        Products = _context.Products.ToList();
     }
 }
