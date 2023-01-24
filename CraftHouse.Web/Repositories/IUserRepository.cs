5a31a75fc77fdc10e935c315213c5fc7bee218fc
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-01-24 22:50:49 +0100

    feat: add user repo
    Add user repository for better organised user management.

---
+++ CraftHouse.Web/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using CraftHouse.Web.Entities;
+
+namespace CraftHouse.Web.Repositories;
+
+public interface IUserRepository
+{
+    User? GetUserById(int id);
+    User? GetUserByEmail(string email);
+}
