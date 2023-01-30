757bf5d6b644595d5e6c56f80e5f2c53663d7cf6
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-01-30 17:50:18 +0100

    feat: add create method to UserRepository

--- CraftHouse.Web/Repositories/UserRepository.cs
+++ CraftHouse.Web/Repositories/UserRepository.cs
@@ -1,15 +1,20 @@
 ﻿using CraftHouse.Web.Data;
 using CraftHouse.Web.Entities;
+using CraftHouse.Web.Helpers;
+using CraftHouse.Web.Services;
+using FluentValidation;
 
 namespace CraftHouse.Web.Repositories;
 
 public class UserRepository : IUserRepository
 {
    private readonly AppDbContext _context;
+   private readonly IValidator<User> _validator;
 
-   public UserRepository(AppDbContext context)
+   public UserRepository(AppDbContext context,IValidator<User> validator)
    {
       _context = context;
+      _validator = validator;
    }
    
    public User? GetUserById(int id)
@@ -27,6 +32,40 @@ public class UserRepository : IUserRepository
    public IEnumerable<User> Get()
    {
       return _context.Users.Where(x => x.DeletedAt == null).OrderBy(x => x.Id);
+   }
+
+   public async Task<Result> CreateAsync(User user, string password)
+   {
+      var validationResult = await _validator.ValidateAsync(user);
+      if (!validationResult.IsValid)
+      {
+         return new Result
+         {
+            Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList(),
+            Succeeded = false
+         };
+      }
+      
+      var isUserAlreadyInDb = _context.Users.Any(x => x.Email == user.Email && x.DeletedAt == null);
+      if (isUserAlreadyInDb)
+      {
+         return new Result
+         {
+            Errors = new List<string> { "User with given email already exists." },
+            Succeeded = false
+         };
+      }
+
+      var passwordSalt = HashingHelper.CreateSalt();
+      var passwordHash = HashingHelper.HashPassword(password, passwordSalt);
+      
+      user.PasswordHash = Convert.ToBase64String(passwordHash);
+      user.PasswordSalt = Convert.ToBase64String(passwordSalt);
+
+      _context.Users.Add(user);
+      await _context.SaveChangesAsync();
+
+      return new Result { Succeeded = true };
    }
 
    public async Task UpdateUserAsync(User user)
