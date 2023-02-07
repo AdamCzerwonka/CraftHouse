2079615f076e3088eddc3e3ac72aba5c4232d23e
Marcel Badek <kriollik@gmail.com>
2023-02-07 17:04:54 +0100

    Feat: Option management pages

---
+++ CraftHouse.Web/Pages/Admin/OptionValuesManagement.cshtml.cs
@@ -0,0 +1,116 @@
+﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.Entities;
+using CraftHouse.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CraftHouse.Web.Pages.Admin;
+
+[RequireAuth(UserType.Administrator)]
+public class OptionValuesManagement : PageModel
+{
+    private readonly AppDbContext _context;
+
+    public OptionValuesManagement(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [BindProperty]
+    public int OptionId { get; set; }
+    
+    [BindProperty]
+    public int ProductId { get; set; }
+
+    [BindProperty]
+    public string OptionValue { get; set; } = null!;
+    
+    [BindProperty]
+    public string OldOptionValue { get; set; } = null!;
+    
+    [BindProperty]
+    public float OptionPrice { get; set; }
+    
+    [BindProperty]
+    public string OptionName { get; set; } = null!;
+
+    public List<OptionValue> OptionValues { get; set; } = null!;
+
+
+
+    public void OnGet(int optionId)
+    {
+        OptionId = optionId;
+        OptionValues = _context.OptionValues.Where(x => x.OptionId == OptionId).ToList();
+        OptionName = _context.Options.FirstOrDefault(x => x.Id == OptionId)!.Name;
+    }
+
+    public async Task<IActionResult> OnPostRemoveAsync()
+    {
+        OptionValues = _context.OptionValues.Where(x => x.OptionId == OptionId).ToList();
+
+        foreach (var option in OptionValues)      
+        {
+            if (option.OptionId == OptionId && option.Value == OptionValue)
+            {
+                _context.OptionValues.Remove(option);
+            }
+        }
+
+        await _context.SaveChangesAsync();
+
+        return Redirect($"/admin/OptionValuesManagement/{OptionId}");
+    }
+    
+    public async Task<IActionResult> OnPostOptionNameAsync()
+    {
+        var option = _context.Options.FirstOrDefault(x => x.Id == OptionId);
+
+        option!.Name = OptionName;
+
+        _context.Options.Update(option);
+        await _context.SaveChangesAsync();
+
+        return Redirect($"/admin/OptionValuesManagement/{OptionId}");
+    }
+    
+    public async Task<IActionResult> OnPostNewValueAsync()
+    {
+        var option = _context.Options.FirstOrDefault(x => x.Id == OptionId);
+
+        var newValue = new OptionValue()
+        {
+            Value = OptionValue,
+            Price = OptionPrice,
+            Option = option!
+        };
+
+        await _context.OptionValues.AddAsync(newValue);
+        await _context.SaveChangesAsync();
+
+        return Redirect($"/admin/OptionValuesManagement/{OptionId}");
+    }
+    
+    public async Task<IActionResult> OnPostUpdateValueAsync()
+    {
+        var value = _context.OptionValues.FirstOrDefault(x => x.OptionId == OptionId && x.Value == OldOptionValue);
+
+        _context.OptionValues.Remove(value!);
+
+        var option = _context.Options.FirstOrDefault(x => x.Id == OptionId);
+
+        var newValue = new OptionValue()
+        {
+            Value = OptionValue,
+            Price = OptionPrice,
+            Option = option!
+        };
+        
+        await _context.OptionValues.AddAsync(newValue);
+        await _context.SaveChangesAsync();
+
+        return Redirect($"/admin/OptionValuesManagement/{OptionId}");
+    }
+
+
+}
---
+++ CraftHouse.Web/Pages/Admin/OptionsManagement.cshtml.cs
@@ -0,0 +1,86 @@
+﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.Entities;
+using CraftHouse.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CraftHouse.Web.Pages.Admin;
+
+[RequireAuth(UserType.Administrator)]
+public class OptionsManagement : PageModel
+{
+    private readonly AppDbContext _context;
+
+    public OptionsManagement (AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [BindProperty]
+    public string Name { get; set; } = null!;
+
+    [BindProperty]
+    public int ProductId { get; set; }
+    
+    [BindProperty]
+    public int OptionId { get; set; }
+    
+    [BindProperty]
+    public int MaxOccurs { get; set; }
+
+    [BindProperty]
+    public List<string> OptionValues { get; set; } = null!;
+
+    [BindProperty]
+    public List<float> OptionPrices { get; set; } = null!;
+    
+    public List<Option> ExistingOptions { get; set; } = null!;
+
+    public int OptionNumber { get; set; }
+
+    public void OnGet(int productId, int optionNumber)
+    {
+        OptionNumber = optionNumber;
+        ProductId = productId;
+        ExistingOptions = _context.Options.Where(x => x.Products.Any(x => x.Id == ProductId)).Where(x => x.DeletedAt == null).ToList();
+    }
+    
+    public async Task<IActionResult> OnPostOptionAsync()
+    {
+        var product = _context.Products.FirstOrDefault(x => x.Id == ProductId);
+
+        ICollection<OptionValue> optionValues = OptionValues.Select((t, i) => new OptionValue() { Value = t, Price = OptionPrices[i] }).ToList();
+
+        var option = new Option()
+        {
+            Name = Name,
+            MaxOccurs = MaxOccurs,
+            OptionValues = optionValues,
+            Products = new[] { product! }
+        };
+
+        await _context.Options.AddAsync(option);
+        await _context.SaveChangesAsync();
+
+        return Redirect($"/admin/OptionsManagement/{ProductId}?optionNumber={1}");
+    }
+
+    public async Task<IActionResult> OnPostRemoveAsync()
+    {
+        ExistingOptions = _context.Options.Where(x => x.Products.Any(x => x.Id == ProductId)).Where(x => x.DeletedAt == null).ToList();
+
+        foreach (var option in ExistingOptions)
+        {
+            if (option.Id != OptionId) continue;
+            option.UpdatedAt = DateTime.Now;
+            option.DeletedAt = DateTime.Now;
+            _context.Options.Update(option);
+            break;
+        }
+        
+        await _context.SaveChangesAsync();
+        
+        return Redirect($"/admin/OptionsManagement/{ProductId}?optionNumber={1}");
+    }
+}
