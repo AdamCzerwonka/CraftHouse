beaf40c46f2cae7bcbefbdc0a550275a4a150e14
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-02-07 15:33:44 +0100

    feat: make Index pagination use async versions

--- CraftHouse.Web/Pages/Index.cshtml.cs
+++ CraftHouse.Web/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using CraftHouse.Web.Entities;
 using CraftHouse.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CraftHouse.Web.Pages;
 
@@ -10,23 +11,19 @@ public class IndexModel : PageModel
 {
     private readonly IAuthService _authService;
     private readonly AppDbContext _context;
-    private readonly ILogger<Index> _logger;
 
-    public IndexModel(IAuthService authService, AppDbContext context, ILogger<Index> logger)
+    public IndexModel(IAuthService authService, AppDbContext context)
     {
         _authService = authService;
         _context = context;
-        _logger = logger;
     }
 
     public User? LoggedInUser { get; set; }
     public List<Product> Products { get; set; } = null!;
-
     public int PageNumber { get; set; }
 
     public async Task<IActionResult> OnGetAsync(int pageNumber, CancellationToken cancellationToken)
     {
-        _logger.LogWarning("Page number: {@number}", pageNumber);
         LoggedInUser = await _authService.GetLoggedInUserAsync(cancellationToken);
 
         if (pageNumber <= 0)
@@ -38,15 +35,18 @@ public class IndexModel : PageModel
         const int productsPerPage = 15;
         var toSkip = productsPerPage * (pageNumber - 1);
 
-        Products = _context
+        var query = _context
             .Products
             .Where(x => x.DeletedAt == null)
-            .OrderBy(x => x.Id)
+            .OrderBy(x => x.Id);
+
+        var productCount = await query.CountAsync(cancellationToken);
+
+        Products = await query
             .Skip(toSkip)
             .Take(productsPerPage)
-            .ToList();
+            .ToListAsync(cancellationToken);
 
-        var productCount = _context.Products.Count(x => x.DeletedAt == null);
         ViewData["lastPageNumber"] = 1 + productCount / productsPerPage;
 
         if (Products.Count == 0)
