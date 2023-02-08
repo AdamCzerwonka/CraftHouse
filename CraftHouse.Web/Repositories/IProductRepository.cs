fd5d940168b14bb6490c3d2b0549d02bebf4b5ea
Marcel Badek <kriollik@gmail.com>
2023-02-08 21:19:19 +0100

    Feat: Add product repository

---
+++ CraftHouse.Web/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using CraftHouse.Web.Entities;
+
+namespace CraftHouse.Web.Repositories;
+
+public interface IProductRepository
+{
+    Task<List<Product>> GetProductsAsync(CancellationToken cancellationToken);
+    Task<Product?> GetProductByIdAsync(int id, CancellationToken cancellationToken);
+    Task AddProductAsync(Product product, CancellationToken cancellationToken);
+    Task DeleteProductWithOptionsAsync(Product product, CancellationToken cancellationToken);
+}
---
+++ CraftHouse.Web/Repositories/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CraftHouse.Web.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;
+    private readonly IOptionRepository _optionRepository;
+
+    public ProductRepository(AppDbContext context, IOptionRepository optionRepository)
+    {
+        _context = context;
+        _optionRepository = optionRepository;
+    }
+    
+    public async Task<List<Product>> GetProductsAsync(CancellationToken cancellationToken)
+    => await _context
+        .Products
+        .Include(x => x.Category)
+        .Where(x => x.DeletedAt == null)
+        .AsNoTracking()
+        .ToListAsync(cancellationToken);
+
+    public async Task<Product?> GetProductByIdAsync(int id, CancellationToken cancellationToken)
+    => await _context
+        .Products
+        .Where(x => x.DeletedAt == null)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+    public async Task AddProductAsync(Product product, CancellationToken cancellationToken)
+    {
+        product.CreatedAt = DateTime.Now;
+        product.UpdatedAt = DateTime.Now;
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteProductWithOptionsAsync(Product product, CancellationToken cancellationToken)
+    {
+        var options = await _optionRepository.GetProductOptionsAsync(product, cancellationToken);
+
+        foreach (var option in options!.Options)
+        {
+            await _optionRepository.DeleteOptionAsync(option, cancellationToken);
+        }
+        
+        product.UpdatedAt = DateTime.Now;
+        product.DeletedAt = DateTime.Now;
+
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
