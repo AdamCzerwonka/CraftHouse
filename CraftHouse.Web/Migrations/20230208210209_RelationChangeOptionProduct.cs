4b258a139eb29fc694378b18462b13cb72d943ff
Marcel Badek <kriollik@gmail.com>
2023-02-08 22:08:36 +0100

    Feat: Migration files for changed relation between option and product

---
+++ CraftHouse.Web/Migrations/20230208210209_RelationChangeOptionProduct.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CraftHouse.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationChangeOptionProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OptionProduct");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Options",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Options_ProductId",
+                table: "Options",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Options_Products_ProductId",
+                table: "Options",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Options_Products_ProductId",
+                table: "Options");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Options_ProductId",
+                table: "Options");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Options");
+
+            migrationBuilder.CreateTable(
+                name: "OptionProduct",
+                columns: table => new
+                {
+                    OptionsId = table.Column<int>(type: "int", nullable: false),
+                    ProductsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OptionProduct", x => new { x.OptionsId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_OptionProduct_Options_OptionsId",
+                        column: x => x.OptionsId,
+                        principalTable: "Options",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OptionProduct_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OptionProduct_ProductsId",
+                table: "OptionProduct",
+                column: "ProductsId");
+        }
+    }
+}
