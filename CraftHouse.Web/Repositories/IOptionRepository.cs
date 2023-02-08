588cefd475561e34832b9c08f7c191ff863c5ba5
Marcel Badek <kriollik@gmail.com>
2023-02-08 21:19:09 +0100

    Feat: Add option repository

---
+++ CraftHouse.Web/Repositories/IOptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using CraftHouse.Web.Entities;
+
+namespace CraftHouse.Web.Repositories;
+
+public interface IOptionRepository
+{
+    Task<Product?> GetProductOptionsAsync(Product product, CancellationToken cancellationToken);
+    Task DeleteOptionAsync(Option option, CancellationToken cancellationToken);
+}
---
+++ CraftHouse.Web/Repositories/OptionRepository.cs
@@ -0,0 +1,28 @@
+﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CraftHouse.Web.Repositories;
+
+public class OptionRepository : IOptionRepository
+{
+    private readonly AppDbContext _context;
+
+    public OptionRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Product?> GetProductOptionsAsync(Product product, CancellationToken cancellationToken)
+     => await _context
+         .Products
+         .Include(x => x.Options)
+         .FirstOrDefaultAsync(x => x.Id == product.Id, cancellationToken);
+
+    public async Task DeleteOptionAsync(Option option, CancellationToken cancellationToken)
+    {
+        option.UpdatedAt = DateTime.Now;
+        option.DeletedAt = DateTime.Now;
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
