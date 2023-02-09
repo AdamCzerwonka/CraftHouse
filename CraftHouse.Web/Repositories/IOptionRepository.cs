8688ed4249c17e2ac2294e3a4b1443398237e925
Marcel Badek <kriollik@gmail.com>
2023-02-09 14:48:52 +0100

    Feat: New methods in option repository

--- CraftHouse.Web/Repositories/IOptionRepository.cs
+++ CraftHouse.Web/Repositories/IOptionRepository.cs
@@ -5,7 +5,11 @@ namespace CraftHouse.Web.Repositories;
 public interface IOptionRepository
 {
     Task<Product?> GetProductOptionsAsync(Product product, CancellationToken cancellationToken);
+    Task<Option?> GetOptionByIdAsync(int id, CancellationToken cancellationToken);
+    Task AddOptionAsync(Option option, CancellationToken cancellationToken);
     Task DeleteOptionAsync(Option option, CancellationToken cancellationToken);
     Task<List<Option>> GetOptionsByProductAsync(Product product, CancellationToken cancellationToken);
+    Task<List<Option>> GetOptionsByProductIdAsync(int productId, CancellationToken cancellationToken);
     Task<bool> IsOptionDeletedAsync(Option option, CancellationToken cancellationToken);
+    Task UpdateOptionAsync(Option option, CancellationToken cancellationToken);
 }
