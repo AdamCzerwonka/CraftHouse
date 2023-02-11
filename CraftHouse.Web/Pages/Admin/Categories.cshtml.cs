ea6d390d6def150131e508bd4c998968dc47c285
Marcel Badek <kriollik@gmail.com>
2023-02-11 21:25:01 +0100

    Feat: Using CategoryValidator in category pages

--- CraftHouse.Web/Pages/Admin/Categories.cshtml.cs
+++ CraftHouse.Web/Pages/Admin/Categories.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.DTOs;
 using CraftHouse.Web.Entities;
 using CraftHouse.Web.Infrastructure;
 using CraftHouse.Web.Repositories;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,18 +13,18 @@ namespace CraftHouse.Web.Pages.Admin;
 public class CategoryManagement : PageModel
 {
     private readonly ICategoryRepository _categoryRepository;
+    private readonly IValidator<Category> _validator;
 
-    public CategoryManagement(ICategoryRepository categoryRepository)
+    public CategoryManagement(ICategoryRepository categoryRepository, IValidator<Category> validator)
     {
         _categoryRepository = categoryRepository;
+        _validator = validator;
     }
     
-    public List<Category> Categories { get; set; } = null!;
-
-    public string? Error { get; set; }
-
     [BindProperty]
-    public string CategoryName { get; set; } = null!;
+    public CategoryDto CategoryDto { get; set; } = null!;
+    public List<Category> Categories { get; set; } = null!;
+    public string? Error { get; set; }
 
     public async Task<IActionResult> OnGet(CancellationToken cancellationToken)
     {
@@ -33,25 +35,26 @@ public class CategoryManagement : PageModel
     
     public async Task<IActionResult> OnPostAddCategoryAsync(CancellationToken cancellationToken)
     {
-        var isCategoryExisting = await _categoryRepository.CategoryExistsAsync(CategoryName, cancellationToken);
-        
-        if (CategoryName.Length < 3 || isCategoryExisting)
+        var isCategoryExisting = await _categoryRepository.CategoryExistsAsync(CategoryDto.Name, cancellationToken);
+
+        if (isCategoryExisting)
         {
             Error = "That named category already exits";
-            if (CategoryName.Length < 3)
-            {
-                Error = "Incorrect category name";
-            }
-
             Categories = await _categoryRepository.GetCategoriesAsync(cancellationToken);
-            CategoryName = "";
+            CategoryDto.Name = "";
             return Page();
         }
+        
+        var category = CategoryDto.MapToCategory();
+        var validationResult = await _validator.ValidateAsync(category, cancellationToken);
 
-        var category = new Category
+        if (!validationResult.IsValid)
         {
-            Name = CategoryName
-        };
+            Error = validationResult.Errors.First().ToString();
+            Categories = await _categoryRepository.GetCategoriesAsync(cancellationToken);
+            CategoryDto.Name = "";
+            return Page();
+        }
         
         await _categoryRepository.AddCategoryAsync(category, cancellationToken);
 
