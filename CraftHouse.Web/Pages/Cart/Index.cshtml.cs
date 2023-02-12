cb2846f649704a780845fbf81d3e730c4f54aadf
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-02-12 19:45:16 +0100

    feat: display option and values on cart page

--- CraftHouse.Web/Pages/Cart/Index.cshtml.cs
+++ CraftHouse.Web/Pages/Cart/Index.cshtml.cs
@@ -18,27 +18,79 @@ public class Index : PageModel
     }
 
     public IEnumerable<CartEntryProduct> CartEntries { get; set; } = null!;
-    
+
     public async Task OnGetAsync(CancellationToken cancellationToken)
     {
         var entries = _cartService.GetCartEntries();
-        var productIds = entries.Select(x => x.ProductId);
-        var prods = await _context
-            .Products
-            .AsNoTracking()
-            .Where(x => productIds.Contains(x.Id))
-            .Select(x=> new CartEntryProduct()
-            {
-                Name = x.Name,
-                Price = x.Price
-            })
-            .ToListAsync(cancellationToken);
+        var cartProducts = new List<CartEntryProduct>();
 
-        CartEntries = prods;
+        foreach (var entry in entries)
+        {
+            var cartProduct = new CartEntryProduct();
+
+            var product = await _context
+                .Products
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == entry.ProductId && x.DeletedAt == null, cancellationToken);
+
+            if (entry.Options is not null)
+            {
+                foreach (var option in entry.Options)
+                {
+                    var opt = await _context
+                        .Options
+                        .Include(x=>x.OptionValues)
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.Id == option.OptionId, cancellationToken);
+
+
+                    var cartOption = new CartOption()
+                    {
+                        Name = opt!.Name
+                    };
+                    
+                    foreach (var value in option.Values)
+                    {
+                        var val = opt.OptionValues.FirstOrDefault(x => x.Id == value);
+
+                        var optionValue = new CartOptionValue()
+                        {
+                            Name = val.Value,
+                            Price = val.Price
+                        };
+                        
+                        cartOption.Values.Add(optionValue);
+                    }
+
+                    cartProduct.Options.Add(cartOption);
+                }
+            }
+
+            cartProduct.Name = product!.Name;
+            cartProduct.Price = product!.Price;
+
+            cartProducts.Add(cartProduct);
+        }
+
+        CartEntries = cartProducts;
     }
 }
 
 public class CartEntryProduct
+{
+    public string Name { get; set; } = null!;
+    public float Price { get; set; }
+
+    public List<CartOption> Options { get; set; } = new();
+}
+
+public class CartOption
+{
+    public string Name { get; set; } = null!;
+    public List<CartOptionValue> Values { get; set; } = new();
+}
+
+public class CartOptionValue
 {
     public string Name { get; set; } = null!;
     public float Price { get; set; }
