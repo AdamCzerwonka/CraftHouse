c2e610da72df5e0c3a2b2d951af3637a3bc999d1
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-02-12 19:44:43 +0100

    ref: make file more readable

--- CraftHouse.Web/Pages/Cart/Add.cshtml.cs
+++ CraftHouse.Web/Pages/Cart/Add.cshtml.cs
@@ -18,7 +18,8 @@ public class Add : PageModel
     private readonly AppDbContext _context;
     private readonly ILogger<Add> _logger;
 
-    public Add(AppDbContext context, ILogger<Add> logger, IProductRepository productRepository, ICartService cartService)
+    public Add(AppDbContext context, ILogger<Add> logger, IProductRepository productRepository,
+        ICartService cartService)
     {
         _productRepository = productRepository;
         _cartService = cartService;
@@ -29,7 +30,7 @@ public class Add : PageModel
     public Product Product { get; set; } = null!;
 
     public List<Option> Options { get; set; } = null!;
-    
+
     public async Task OnGetAsync(int productId, CancellationToken cancellationToken)
     {
         var product = await _context
@@ -39,7 +40,7 @@ public class Add : PageModel
 
         Options = await _context
             .Options
-            .Include(x=>x.OptionValues)
+            .Include(x => x.OptionValues)
             .Where(x => x.ProductId == product!.Id)
             .ToListAsync(cancellationToken);
 
@@ -47,80 +48,91 @@ public class Add : PageModel
     }
 
     [BindProperty]
-    public AddToCartModel AddToCart { get; set; }
+    public AddToCartModel CartProduct { get; set; }
 
     public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Options: {@options}", AddToCart);
-        
+        _logger.LogInformation("Options: {@options}", CartProduct);
         // get and validate the product
-        var product = await _productRepository.GetProductByIdAsync(AddToCart.ProductId, cancellationToken);
+        var product = await _productRepository.GetProductByIdAsync(CartProduct.ProductId, cancellationToken);
         if (product is null)
         {
             throw new InvalidOperationException("Product does not exists");
         }
-        
-        // validate option list
 
+        // validate option list
         var optionsInDb = await _context
             .Options
-            .Include(x=>x.OptionValues)
+            .Include(x => x.OptionValues)
             .AsNoTracking()
-            .Where(x => x.ProductId == AddToCart.ProductId && x.DeletedAt == null)
+            .Where(x => x.ProductId == CartProduct.ProductId && x.DeletedAt == null)
             .ToListAsync(cancellationToken);
 
-        List<CartEntryOption>? cartOptions = null;
 
-        if (optionsInDb.Count != 0)
+        var anyOptionRequired = optionsInDb.Any(x => x.Required);
+        if (optionsInDb.Count == 0)
         {
-            cartOptions = new List<CartEntryOption>();
-            foreach (var opt in AddToCart.Options)
+            var cartEntry = new CartEntry()
             {
-                var option = optionsInDb.FirstOrDefault(x => x.Id == opt.OptionId);
-                if (option is null)
-                {
-                    throw new InvalidOperationException("Option does not exits");
-                }
+                ProductId = CartProduct.ProductId,
+            };
 
-                var valuesCount = opt.Values.Count;
+            _cartService.AddCartEntry(cartEntry);
 
-                if (option.Required && valuesCount == 0)
-                {
+            return Redirect("/cart");
+        }
+
+        List<CartEntryOption> cartOptions = new();
+        foreach (var opt in CartProduct.Options)
+        {
+            var option = optionsInDb.FirstOrDefault(x => x.Id == opt.OptionId);
+            if (option is null)
+            {
+                throw new InvalidOperationException("Option does not exits");
+            }
+
+            var valuesCount = opt.Values.Count;
+
+            switch (option.Required)
+            {
+                case true when valuesCount == 0:
                     _logger.LogInformation("Option required but value was not specified");
                     throw new InvalidOperationException("Option required");
-                }
-
-                if (option.MaxOccurs < valuesCount)
-                {
-                    _logger.LogError("Option max occurrences were exceeded");
-                    throw new InvalidOperationException("Too much values");
-                }
-
-                var cartOption = new CartEntryOption()
-                {
-                    OptionId = option.Id
-                };
-
-                foreach (var val in opt.Values.Select(value => option.OptionValues.FirstOrDefault(x => x.Id == value)))
-                {
-                    if (val is null)
-                    {
-                        throw new InvalidOperationException("OptionValue does not exits");
-                    }
-
-                    cartOption.Options.Add(val.Id);
-                }
-                
-                cartOptions.Add(cartOption);
+                case false when valuesCount == 0:
+                    continue;
             }
+
+            if (option.MaxOccurs < valuesCount)
+            {
+                _logger.LogError("Option max occurrences were exceeded");
+                throw new InvalidOperationException("Too much values");
+            }
+
+            var cartOption = new CartEntryOption()
+            {
+                OptionId = option.Id,
+                Values = new List<int>()
+            };
+
+            foreach (var val in opt.Values.Select(value => option.OptionValues.FirstOrDefault(x => x.Id == value)))
+            {
+                if (val is null)
+                {
+                    throw new InvalidOperationException("OptionValue does not exits");
+                }
+
+                cartOption.Values.Add(val.Id);
+            }
+
+            cartOptions.Add(cartOption);
         }
 
         var entry = new CartEntry()
         {
-            ProductId = AddToCart.ProductId,
+            ProductId = CartProduct.ProductId,
             Options = cartOptions
         };
-        
+
         _cartService.AddCartEntry(entry);
 
         return Redirect("/cart");
@@ -129,12 +141,14 @@ public class Add : PageModel
 
 public class AddToCartModel
 {
-    public int ProductId { get; set; }
-    public List<CartOptionValueModel> Options { get; set; } = null!;
+    public int ProductId { get; init; }
+    public List<AddToCartOptionModel> Options { get; init; } = null!;
 }
 
-public class CartOptionValueModel
+public class AddToCartOptionModel
 {
-    public int OptionId { get; set; }
-    public List<int> Values { get; set; } = null!;
+    public int OptionId { get; init; }
+    // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
+    // ReSharper disable once CollectionNeverUpdated.Global
+    public List<int> Values { get; init; } = new();
 }
