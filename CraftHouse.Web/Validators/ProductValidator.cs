61bb74d3b3762a2afa10f5b1c9dc2da928202e4c
Marcel Badek <kriollik@gmail.com>
2023-02-12 12:32:00 +0100

    Feat: Product validator and dto

---
+++ CraftHouse.Web/Validators/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using CraftHouse.Web.Entities;
+using FluentValidation;
+
+namespace CraftHouse.Web.Validators;
+
+public class ProductValidator : AbstractValidator<Product>
+{
+    public ProductValidator()
+    {
+        RuleFor(product => product.Name).NotEmpty().MinimumLength(3);
+        RuleFor(product => product.IsAvailable).NotNull();
+        RuleFor(product => product.Price).NotEmpty().Must(BeAValidPrice);
+        RuleFor(product => product.Description).NotEmpty().MinimumLength(3);
+    }
+
+    private static bool BeAValidPrice(float price)
+    {
+        var toCheck = price.ToString();
+        if (toCheck.IndexOf(",") == -1) return true;
+        var precision = toCheck.Length - toCheck.IndexOf(",") - 1;
+        return precision is 2 or 1;
+    }
+}
