5710ff875a31b8b937e6a0d871ef614da6ea44c7
Marcel Badek <kriollik@gmail.com>
2023-02-12 13:54:43 +0100

    Feat: Add option and option value validators and dtos

---
+++ CraftHouse.Web/DTOs/OptionValueDto.cs
@@ -0,0 +1,22 @@
+﻿using CraftHouse.Web.Entities;
+
+namespace CraftHouse.Web.DTOs;
+
+public class OptionValueDto
+{
+    public string Value { get; set; } = null!;
+
+    public float Price { get; set; }
+
+    public int OptionId { get; set; }
+
+    public OptionValue MapToOptionValue()
+    {
+        return new OptionValue()
+        {
+            Value = Value,
+            Price = Price,
+            OptionId = OptionId
+        };
+    }
+}
---
+++ CraftHouse.Web/Validators/OptionValueValidator.cs
@@ -0,0 +1,15 @@
+﻿using CraftHouse.Web.Entities;
+using FluentValidation;
+
+namespace CraftHouse.Web.Validators;
+
+public class OptionValueValidator : AbstractValidator<OptionValue>
+{
+    public OptionValueValidator()
+    {
+        RuleFor(optionValue => optionValue.Value).NotEmpty().MinimumLength(3);
+        RuleFor(optionValue => optionValue.Price).NotEmpty().Must(ValidationMethods.IsPriceValid);
+        RuleFor(optionValue => optionValue.OptionId).NotEmpty().GreaterThan(0);
+        
+    }
+}
