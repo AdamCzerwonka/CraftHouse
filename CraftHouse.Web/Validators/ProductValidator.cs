d756ba92eff3c5bac75ebb22020abf79460e94d7
Marcel Badek <kriollik@gmail.com>
2023-02-12 13:55:02 +0100

    Feat: Option and option values files uses validator and dtos

--- CraftHouse.Web/Validators/ProductValidator.cs
+++ CraftHouse.Web/Validators/ProductValidator.cs
@@ -9,15 +9,8 @@ public class ProductValidator : AbstractValidator<Product>
     {
         RuleFor(product => product.Name).NotEmpty().MinimumLength(3);
         RuleFor(product => product.IsAvailable).NotNull();
-        RuleFor(product => product.Price).NotEmpty().Must(BeAValidPrice);
+        RuleFor(product => product.Price).NotEmpty().GreaterThanOrEqualTo(0).Must(ValidationMethods.IsPriceValid);
         RuleFor(product => product.Description).NotEmpty().MinimumLength(3);
-    }
-
-    private static bool BeAValidPrice(float price)
-    {
-        var toCheck = price.ToString();
-        if (toCheck.IndexOf(",") == -1) return true;
-        var precision = toCheck.Length - toCheck.IndexOf(",") - 1;
-        return precision is 2 or 1;
+        RuleFor(product => product.CategoryId).NotEmpty().GreaterThan(0);
     }
 }
