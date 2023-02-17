23fe7b8fd9936805ae36a014b3cae091cdb2fee5
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-02-17 22:12:23 +0100

    feat: add sorting to main page

--- CraftHouse.Web/Pages/Index.cshtml.cs
+++ CraftHouse.Web/Pages/Index.cshtml.cs
@@ -13,18 +13,22 @@ public class IndexModel : PageModel
     private readonly AppDbContext _context;
     private readonly ICategoryRepository _categoryRepository;
 
-    public IndexModel(IAuthService authService, AppDbContext context, ICategoryRepository categoryRepository, ILogger<IndexModel> logger)
+    public IndexModel(IAuthService authService, AppDbContext context, ICategoryRepository categoryRepository,
+        ILogger<IndexModel> logger)
     {
         _context = context;
         _categoryRepository = categoryRepository;
     }
 
     public int? CategoryId { get; set; }
+    public int PageNumber { get; set; }
+    public string? Order { get; set; }
+    public bool? IsAscending { get; set; }
     public List<Category> Categories { get; set; } = null!;
     public List<Product> Products { get; set; } = null!;
-    public int PageNumber { get; set; }
 
-    public async Task<IActionResult> OnGetAsync(int pageNumber, int? category, CancellationToken cancellationToken)
+    public async Task<IActionResult> OnGetAsync(int pageNumber, int? category, string? order, bool? isAscending,
+        CancellationToken cancellationToken)
     {
         if (pageNumber <= 0)
         {
@@ -34,6 +38,8 @@ public class IndexModel : PageModel
         CategoryId = category;
         Categories = await _categoryRepository.GetCategoriesAsync(cancellationToken);
         PageNumber = pageNumber;
+        Order = order;
+        IsAscending = isAscending;
         const int productsPerPage = 15;
         var toSkip = productsPerPage * (pageNumber - 1);
 
@@ -50,8 +56,19 @@ public class IndexModel : PageModel
 
         var productCount = await query.CountAsync(cancellationToken);
 
+        order = order?.ToLower();
+        isAscending ??= true;
+
+        query = (order, isAscending) switch
+        {
+            ("name", true) => query.OrderBy(x => x.Name),
+            ("price", true) => query.OrderBy(x => x.Price),
+            ("name", false) => query.OrderByDescending(x => x.Name),
+            ("priceDesc", false) => query.OrderByDescending(x => x.Price),
+            _ => query.OrderBy(x => x.Id)
+        };
+
         Products = await query
-            .OrderBy(x=>x.Id)
             .Skip(toSkip)
             .Take(productsPerPage)
             .AsNoTracking()
