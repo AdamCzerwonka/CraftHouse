240a129e58aa0705cdf7c9e44b155d83eabae5b8
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-02-17 22:33:30 +0100

    feat: use product repository in Index
    Use productRepository to filter, sort and count products on main page instead of using DbContext directly.

--- CraftHouse.Web/Pages/Index.cshtml.cs
+++ CraftHouse.Web/Pages/Index.cshtml.cs
@@ -1,23 +1,20 @@
-﻿using CraftHouse.Web.Data;
-using CraftHouse.Web.Entities;
+﻿using CraftHouse.Web.Entities;
 using CraftHouse.Web.Repositories;
-using CraftHouse.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
 namespace CraftHouse.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly AppDbContext _context;
     private readonly ICategoryRepository _categoryRepository;
+    private readonly IProductRepository _productRepository;
 
-    public IndexModel(IAuthService authService, AppDbContext context, ICategoryRepository categoryRepository,
-        ILogger<IndexModel> logger)
+    public IndexModel(ICategoryRepository categoryRepository,
+        ILogger<IndexModel> logger, IProductRepository productRepository)
     {
-        _context = context;
         _categoryRepository = categoryRepository;
+        _productRepository = productRepository;
     }
 
     public int? CategoryId { get; set; }
@@ -41,38 +38,16 @@ public class IndexModel : PageModel
         Order = order;
         IsAscending = isAscending;
         const int productsPerPage = 15;
-        var toSkip = productsPerPage * (pageNumber - 1);
 
-        var query = _context
-            .Products
-            .AsNoTracking()
-            .Where(x => x.DeletedAt == null);
+        var productCount = await _productRepository.GetProductsCountByCategoryAsync(CategoryId, cancellationToken);
 
-        if (category is not null)
-        {
-            query = query
-                .Where(x => x.CategoryId == category);
-        }
-
-        var productCount = await query.CountAsync(cancellationToken);
-
-        order = order?.ToLower();
-        isAscending ??= true;
-
-        query = (order, isAscending) switch
-        {
-            ("name", true) => query.OrderBy(x => x.Name),
-            ("price", true) => query.OrderBy(x => x.Price),
-            ("name", false) => query.OrderByDescending(x => x.Name),
-            ("priceDesc", false) => query.OrderByDescending(x => x.Price),
-            _ => query.OrderBy(x => x.Id)
-        };
-
-        Products = await query
-            .Skip(toSkip)
-            .Take(productsPerPage)
-            .AsNoTracking()
-            .ToListAsync(cancellationToken);
+        Products = await _productRepository
+            .GetProductsByCategoryWithSortingAsync(
+                pageNumber,
+                CategoryId,
+                Order,
+                IsAscending,
+                cancellationToken);
 
         ViewData["lastPageNumber"] = 1 + productCount / productsPerPage;
 
