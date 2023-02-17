c2521322ef824a90f7192b15f74d9ea99b526461
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-02-17 22:32:16 +0100

    feat: add methods to ProductRepository

--- CraftHouse.Web/Repositories/IProductRepository.cs
+++ CraftHouse.Web/Repositories/IProductRepository.cs
@@ -5,6 +5,10 @@ namespace CraftHouse.Web.Repositories;
 public interface IProductRepository
 {
     Task<List<Product>> GetProductsAsync(CancellationToken cancellationToken);
+
+    Task<List<Product>> GetProductsByCategoryWithSortingAsync(int pageNumber, int? categoryId, string? sortBy,
+        bool? isAscending, CancellationToken cancellationToken = default);
+    Task<int> GetProductsCountByCategoryAsync(int? categoryId, CancellationToken cancellationToken);
     Task<Product?> GetProductByIdAsync(int id, CancellationToken cancellationToken);
     Task AddProductAsync(Product product, CancellationToken cancellationToken);
     Task DeleteProductWithOptionsAsync(Product product, CancellationToken cancellationToken);
