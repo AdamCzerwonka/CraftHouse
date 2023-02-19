8a4ebb83c3b1ed56f88ba033ef4ce0f924e8fd5f
Marcel Badek <kriollik@gmail.com>
2023-02-19 15:07:00 +0100

    Feat: Add order item option repository

---
+++ CraftHouse.Web/Repositories/OrderItemOptionRepository.cs
@@ -0,0 +1,20 @@
+﻿using CraftHouse.Web.Data;
+using CraftHouse.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CraftHouse.Web.Repositories;
+
+public class OrderItemOptionRepository : IOrderItemOptionRepository
+{
+    private readonly AppDbContext _context;
+
+    public OrderItemOptionRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<OrderItemOption>> GetOrderItemOptionByOrderItemIdAsync(int id,
+        CancellationToken cancellationToken)
+        => await _context.OrderItemOptions.Where(x => x.OrderItemId == id).AsNoTracking()
+            .ToListAsync(cancellationToken);
+}
