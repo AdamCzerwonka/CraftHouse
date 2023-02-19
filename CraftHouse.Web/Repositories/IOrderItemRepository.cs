7635811772706138335b3a06f256f0a6f8c1aed9
Marcel Badek <kriollik@gmail.com>
2023-02-19 15:06:43 +0100

    Feat: Add order item repository

---
+++ CraftHouse.Web/Repositories/IOrderItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using CraftHouse.Web.Entities;
+
+namespace CraftHouse.Web.Repositories;
+
+public interface IOrderItemRepository
+{
+    Task<List<OrderItem>> GetOrderItemByOrderIdAsync(int id, CancellationToken cancellationToken);
+}
