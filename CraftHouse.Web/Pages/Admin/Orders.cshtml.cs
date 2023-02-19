9e2300d6926e3219e42e773680c7be19ff16ad1a
Marcel Badek <kriollik@gmail.com>
2023-02-19 15:10:04 +0100

    Feat: Add page with all users orders for administrator

---
+++ CraftHouse.Web/Pages/Admin/Orders.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using CraftHouse.Web.Entities;
+using CraftHouse.Web.Infrastructure;
+using CraftHouse.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CraftHouse.Web.Pages.Admin;
+
+[RequireAuth(UserType.Administrator)]
+public class Orders : PageModel
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public Orders(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    
+    public  List<Order> OrdersList { get; set; }
+    
+    public async Task<IActionResult> OnGet(CancellationToken cancellationToken)
+    {
+        OrdersList = (await _orderRepository.GetOrdersAsync(cancellationToken));
+
+        return Page();
+    }
+}
