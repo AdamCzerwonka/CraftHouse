297f3fe709b21b7798e242dfbbca9990ee6009d2
Marcel Badek <kriollik@gmail.com>
2023-02-21 17:04:13 +0100

    Feat: Add sorting for orders by columns or user

--- CraftHouse.Web/Pages/Admin/Orders.cshtml.cs
+++ CraftHouse.Web/Pages/Admin/Orders.cshtml.cs
@@ -10,17 +10,53 @@ namespace CraftHouse.Web.Pages.Admin;
 public class Orders : PageModel
 {
     private readonly IOrderRepository _orderRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly ILogger<Orders> _logger;
 
-    public Orders(IOrderRepository orderRepository)
+    public Orders(IOrderRepository orderRepository, IUserRepository userRepository, ILogger<Orders> logger)
     {
         _orderRepository = orderRepository;
+        _userRepository = userRepository;
+        _logger = logger;
     }
     
-    public  List<Order> OrdersList { get; set; }
-    
-    public async Task<IActionResult> OnGet(CancellationToken cancellationToken)
+    [BindProperty]
+    public int? UserId { get; set; }
+
+    public List<Order> OrdersList { get; set; }
+
+    public List<Entities.User> Users { get; set; }
+
+    public string? OrderBy { get; set; }
+    public bool? IsAscending { get; set; } = false;
+
+    public async Task<IActionResult> OnGetAsync(int? userId, string? orderBy, bool? isAscending,
+        CancellationToken cancellationToken)
     {
-        OrdersList = (await _orderRepository.GetOrdersAsync(cancellationToken));
+        OrderBy = orderBy;
+        IsAscending = isAscending;
+        IsAscending ??= false;
+
+        if (userId is not null)
+        {
+            UserId = userId;
+            var user = await _userRepository.GetUserByIdAsync((int)UserId, cancellationToken);
+
+            if (user is null)
+            {
+                throw new NullReferenceException("User does not exists");
+            }
+
+            OrdersList =
+                await _orderRepository.GetOrdersByUserIdWithSortingAsync((int)UserId, OrderBy, IsAscending,
+                    cancellationToken);
+        }
+        else
+        {
+            OrdersList = await _orderRepository.GetOrdersWithSortingAsync(OrderBy, IsAscending, cancellationToken);
+        }
+
+        Users = await _userRepository.GetAllUsersAsync(cancellationToken);
 
         return Page();
     }
